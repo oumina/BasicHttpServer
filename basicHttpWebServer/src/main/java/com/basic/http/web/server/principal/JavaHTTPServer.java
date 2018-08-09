39bdd571c969b4b7d1afe58c7a71392cc0cdbcfc
oumina el hassane <oumina el hassane@LAPTOP-OR7KVFS0>
2018-08-10 01:33:20 +0200

    manage http methods ; get

--- basicHttpWebServer/src/main/java/com/basic/http/web/server/principal/JavaHTTPServer.java
+++ basicHttpWebServer/src/main/java/com/basic/http/web/server/principal/JavaHTTPServer.java
@@ -1,11 +1,21 @@
 package com.basic.http.web.server.principal;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-public class JavaHTTPServer {
+import java.util.Date;
+import java.util.StringTokenizer;
+/**
+ * 
+ * @author oumina el hassane
+ *
+ */
+public class JavaHTTPServer  implements Runnable{  
 	/**
 	 * Server's configuration
 	 */
@@ -25,24 +35,75 @@ public class JavaHTTPServer {
 	/**
 	 * Client connection via Socket class
 	 */
-	private Socket socket ;
+	private Socket connect ;
 	
-	
-	public JavaHTTPServer(Socket socket) {
+	public JavaHTTPServer(Socket connect) {
 		super();
-		this.socket = socket;
+		this.connect = connect;
 	}
 
-	
 
 	public static void main(String[] args) {
 		try {
 			ServerSocket serverConnect = new ServerSocket(PORT);
 			System.out.println("Server strted .\nListening for connections  on port "+PORT + "....\n");
-		//we listening
+		//we listening until the user halths the server 
+			while(true){
+				JavaHTTPServer myServer = new JavaHTTPServer(serverConnect.accept());
+				
+				if(verbose){
+					System.out.println("Connection is opened "+ new Date()+" ");
+				}
+				
+				//Created dedacted thread to manage a client connections
+				Thread thread = new  Thread(myServer) ;
+				thread.start();
+				
+			}
 		} catch (IOException e) {
 			System.err.println("Server Connection error : "+ e.getMessage());
 			e.printStackTrace();
 		}
+	}
+
+
+
+	@Override
+	public void run() {
+		// we manage our particular client connection
+				BufferedReader in = null;
+				PrintWriter out = null;
+				BufferedOutputStream dataOut = null;
+				String fileRequested = null;
+				
+					// we read characters from the client via input stream on the socket
+					try {
+						// we read characters from the client via input stream on the socket
+						in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+						// we get [character output] stream to client (for headers)
+						out = new PrintWriter(connect.getOutputStream());
+						// get [binary output] stream to client (for requested data)
+						dataOut = new BufferedOutputStream(connect.getOutputStream());
+						
+						// get first line of the request from the client
+						String input = in.readLine();
+						
+						// we parse the request with a string tokenizer
+						StringTokenizer parse = new StringTokenizer(input);
+						String method = parse.nextToken().toUpperCase(); // we get the HTTP method of the client
+						// we get file requested
+						fileRequested = parse.nextToken().toLowerCase();
+						
+						// we support only GET and HEAD methods, we check
+						if (!method.equals("GET")  &&  !method.equals("HEAD")) {
+							if (verbose) {
+								System.out.println("501 Not Implemented : " + method + " method.");
+							}
+						}
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						System.out.println(e.getMessage());
+					}
+		
 	}
 }
